a1fb12a2aff092f3903543a5dc57c288c8879b0f
ankushudapure24 <ankushudapure147@gmail.com>
2025-09-15 04:42:44 +0530

    small fixes

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,3 @@
-// const mysql = require("mysql2/promise");
-
-// // Create connection pool
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "ankush_udapure", // your MySQL user
-//   password: "ADsql@25", // your MySQL password
-//   database: "ratings_app",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
-// module.exports = pool;
-
-// backend/db.js
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") }); // go up one level
 
